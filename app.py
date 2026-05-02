3752f95917eeb726abea920f614a1a023b34b8f9
danielramse2006-glitch <danielramse2006@gmail.com>
2026-05-02 13:05:04 -0600

    Solucionando error de arranque en Render: inicializacion de DB

--- app.py
+++ app.py
@@ -183,5 +183,8 @@ def update_status(id):
     db.session.commit()
     return jsonify({'message': 'Estado actualizado'})
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
